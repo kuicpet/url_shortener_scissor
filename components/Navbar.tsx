93c63374707eba91398229e8d8d7df85c361f869
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-05-26 17:08:50 +0100

    ft--updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { AiOutlineScissor } from 'react-icons/ai';
+import { HiOutlineMenuAlt4, HiOutlineX } from 'react-icons/hi';
 import Logo from '../assets/Logo.svg';
 
-const activeClass: string = 'text-[#0065FE]';
-const notActive: string = 'text-[#141414]';
-const Navbar: React.FC = () => {
+interface ILinks {
+  text: string;
+}
+const Links: React.FC<ILinks> = ({ text }) => {
   return (
-    <nav className=" flex lg:justify-between bg-transparent justify-center items-center  px-4">
+    <li className="my-2">
+      <Link
+        className="focus:text-[#0065FE] hover:text-[#0065FE] text-[#141414] font-semibold"
+        href={'/'}
+      >
+        {text}
+      </Link>
+    </li>
+  );
+};
+const Navbar: React.FC = () => {
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+
+  return (
+    <nav className=" flex lg:justify-between bg-white justify-between items-center  px-4 py-1">
       <Link
         href={`/`}
         className="flex items-center justify-between p-2 my-2 mx-5"
       >
         <Image src={Logo} alt="" />
       </Link>
-      <div className="flex flex-initial justify-between w-1/3 text-[#141414] font-semibold">
+      <div className="md:flex flex-initial justify-between w-1/3 text-[#141414] font-semibold hidden">
         <Link className="focus:text-[#0065FE] hover:text-[#0065FE]" href={'/'}>
           My URLs
         </Link>
@@ -32,11 +47,83 @@ const Navbar: React.FC = () => {
           FAQS
         </Link>
       </div>
-      <div className="flex justify-around border w-1/4">
-        <Link className="text-[#0065FE]" href={'/'}>
+      <div className="md:flex justify-around w-1/2 md:w-1/4 hidden">
+        <Link
+          className="text-[#0065FE] font-semibold  px-3 flex items-center justify-center"
+          href={'/'}
+        >
           Login
         </Link>
-        <Link href={'/'}>Try for free</Link>
+        <Link
+          href={'/'}
+          className="text-white font-semibold border-2 px-4 py-2 border-none outline-none rounded-full bg-[#0065FE] lg:w-[8rem]  flex items-center justify-center"
+        >
+          Try for free
+        </Link>
+      </div>
+      <div className="flex relative">
+        {toggleMenu ? (
+          <HiOutlineX
+            fontSize={28}
+            className="text-black md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <HiOutlineMenuAlt4
+            fontSize={28}
+            className="text-black md:hidden cursor-pointer"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {/**Mobile Menu */}
+        {toggleMenu && (
+          <ul className="z-10 text-lg fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-center rounded-md  text-black animate-slide-in  bg-white">
+            <li className="text-xl w-full my-2">
+              <HiOutlineX
+                fontSize={28}
+                className="cursor-pointer animate-slide-out"
+                onClick={() => setToggleMenu(false)}
+              />
+            </li>
+            {['My URLs', 'Features', 'Pricing', 'Analytics', ' FAQSs'].map(
+              (item, i) => (
+                <Links text={item} key={i} />
+              )
+            )}
+
+            <li className="my-2">
+              <Link
+                className="text-[#0065FE] font-semibold  px-3 flex items-center justify-center"
+                href={'/'}
+              >
+                Login
+              </Link>
+            </li>
+            <li className="my-2">
+              <Link
+                href={'/'}
+                className="text-white font-semibold border-2 px-4 py-2 border-none outline-none rounded-full bg-[#0065FE] lg:w-[8rem]  flex items-center justify-center"
+              >
+                Try for free
+              </Link>
+            </li>
+
+            <div className="md:flex justify-around w-1/2 md:w-1/4 hidden">
+              <Link
+                className="text-[#0065FE] font-semibold  px-3 flex items-center justify-center"
+                href={'/'}
+              >
+                Login
+              </Link>
+              <Link
+                href={'/'}
+                className="text-white font-semibold border-2 px-4 py-2 border-none outline-none rounded-full bg-[#0065FE] lg:w-[8rem]  flex items-center justify-center"
+              >
+                Try for free
+              </Link>
+            </div>
+          </ul>
+        )}
       </div>
     </nav>
   );
