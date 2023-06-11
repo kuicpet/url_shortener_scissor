9e898e0dae50e9d36ee9669d25301f5c6d736021
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-06-11 16:18:08 +0100

    ft--updated analytics

--- pages/dashboard/[slug].tsx
+++ pages/dashboard/[slug].tsx
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import axios from 'axios';
+import { Navbar } from '../../components';
 
 const DashboardPage = () => {
   const router = useRouter();
   const { slug } = router.query;
-  const [urlDetails, setUrlDetails] = useState({});
+  const [urlDetails, setUrlDetails] = useState<any>({});
   useEffect(() => {
     const fetchUrls = async () => {
       try {
-        await axios
-          .get(`/api/track/${slug}`)
-          .then((response) => console.log(response.data));
+        await axios.get(`/api/track/${slug}`).then((response) => {
+          console.log(response.data);
+          setUrlDetails(response?.data);
+        });
       } catch (error) {
         console.log(error);
       }
     };
     fetchUrls();
   }, [slug]);
-  return <div>{slug}</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="h-[100vh] w-full flex lg:flex-row flex-col">
+        <div className="flex justify-center lg:w-[18%] lg:h-[92vh]  m-3">
+          <ul className="w-full text-center">
+            <li className="mb-2">
+              <h2 className="text-xl font-semibold">Url Analytics</h2>
+            </li>
+            <li className="flex justify-center p-2 rounded-md bg-[#0065FE] text-white">
+              <Link href={'/'}>{slug}</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col lg:w-[82%] h-[100vh] border border-[#0065FE] m-3 p-2 rounded-md">
+          <h2 className="text-xl font-semibold">Shortened Url Details</h2>
+          <div className="border-2 border-black my-3 rounded-lg">
+            <ul className="p-2">
+              <li className="text-semibold">
+                Original Url: {urlDetails?.data?.originalUrl}
+              </li>
+              <li className="text-semibold">Shortened Url: {slug}</li>
+              <li className="text-semibold">
+                Number of Clicks: {urlDetails?.data?.clicks?.length} clicks
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
