5120a4247e759c2be3841784e0881735059cd450
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-06-21 23:11:39 +0100

    ft--404 page added

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../assets/Logo.svg';
+
+const Notfound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[75vh]  w-full">
+      <div className="my-3">
+        <h1 className="lg:text-5xl text-2xl font-semibold">oops!</h1>
+      </div>
+      <div className="text-center mt-3">
+        <p>We can&apos;t seem to find the page you&apos;re looking for..</p>
+        <Link href="/" className="text-[#0065FE] font-semibold hover:underline">
+          Go back to the homepage
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Notfound;
